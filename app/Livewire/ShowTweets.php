6f1e482603e777efca4a7b9d7be22150b7e595d6
marcoschags <marcoschags@hotmail.com>
2025-08-06 15:48:10 -0300

    Updated 2025-08-06 15:48:09

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 
 class ShowTweets extends Component
 {
+    public $message = 'Apenas um teste';
+
     public function render()
     {
         return view('livewire.show-tweets');
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,3 +1,7 @@
 <div>
    Show Tweets
+    <p>{{ $message }}</p>
+
+    <textarea name="message" id="message" cols="30" rows="10" wire:model="message">Teste área de teste</textarea>
+    <input type="text" name="message" id="message" wire:model="message">
 </div>
