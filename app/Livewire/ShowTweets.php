6768942914fe2f639d36d93b6dabb88762a2642e
marcoschags <marcoschags@hotmail.com>
2025-08-09 15:15:03 -0300

    Updated 2025-08-09 15:15:02

--- app/Livewire/ShowTweets.php
+++ app/Livewire/ShowTweets.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Tweet;
 use Livewire\Component;
 
 class ShowTweets extends Component
 {
+    public $message = 'Apenas um teste -> ';
+
     public function render()
     {
-        return view('livewire.show-tweets');
+        // $tweets = Tweet::get();
+        $tweets = Tweet::with('user')->get();
+        return view('livewire.show-tweets', compact('tweets'));
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -1,3 +1,20 @@
 <div>
    Show Tweets
+
+  <p>{{ $message }}</p>
+
+  <textarea name="message" id="message" cols="30" rows="10">{{ $message }}</textarea>
+  <input type="text" name="message" id="message" wire:model="message" />
+
+  <hr>
+
+  @foreach ($tweets as $tweet)
+    <div class="tweet">
+      <h3>{{ $tweet->user->name }}</h3>
+      <p>{{ $tweet->content }}</p>
+      <small>
+        Posted at: {{ $tweet->created_at ? $tweet->created_at->diffForHumans() : 'Data indisponível' }}
+      </small>
+    </div>
+  @endforeach
 </div>
